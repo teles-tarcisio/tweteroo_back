7e6cafd858912995159dffee6e410c39f2610ac8
Tarcísio Teles <tfst@ecomp.poli.br>
2022-01-23 14:21:08 -0300

    test: validating all requirements (wip)

--- src/server.js
+++ src/server.js
@@ -1,84 +1,53 @@
 import express from 'express';
 import cors from 'cors';
 
+import { usersArray, tweetsArray, latestTenTweets, getUserAvatar, checkRequestInput, formatInputData } from './services.js';
+
 const server = express();
 server.use(express.json());
 server.use(cors());
 
-const usersArray = [
-  {
-    username: "hedgy",
-    avatar: "https://i.pinimg.com/736x/2d/00/2d/2d002d1e5aa86a0b274c4a9bf8a87b53--bubble-baths-hedge-hog.jpg"
-  },
-  {
-    username: "bobesponja",
-    avatar: "https://avatarfiles.alphacoders.com/833/thumb-1920-83315.png"
-  }
-];
-
-const tweetsArray = [
-  {
-    username: "0000",
-    tweet:"tweet 0000",
-    avatar: "https://image.shutterstock.com/image-photo/black-number-0000-on-white-260nw-2001074579.jpg"
-  },
-  {
-    username: "0001",
-    tweet:"tweet 0001",
-    avatar: "https://i.ytimg.com/vi/HYE6E3MYyAc/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLAJagHxrSk7wxUZmr9kGmnK2WbTRg"
-  },
-];
-
-function latestTenTweets(targetArray) {
-  const newArray = [];
-  for (let i = targetArray.length - 1; i > targetArray.length - 11; i--) {
-    newArray.push(targetArray[i]);
-  }
-  return newArray;
-
-  /* // >>> SLICE vs SPLICE !!!
-  const testArray = [
-    { id:1, name:"01" }, { id:2, name:"02"}, { id:3, name:"03"}, { id:4, name:"04"}, { id:5, name:"05"}, { id:6, name:"06"}];
-  
-  console.log(testArray.slice(testArray.length -6, testArray.length));
-  */
-}
-
-function getUserAvatar(targetUserName) {
-  const targetUserObj = usersArray.find( user => (
-    user.username === targetUserName )
-  );
-  
-  return targetUserObj.avatar;
-}
 
 server.post('/sign-up', (req, resp) => {
-  
-  usersArray.push(req.body);
-  resp.send('OK');
+  if (checkRequestInput(req)) {
+    const validInputData = formatInputData(req);
+    usersArray.push( {
+      username: validInputData.username,
+      avatar: validInputData.avatar
+     });
+    resp.status(201).send('OK');
+    console.log('SIGNUP: ', usersArray);
+  }
+  else {
+    resp.status(400).send("Todos os campos são obrigatórios!");
+    console.log('invalid signup data!');
+  }
 });
 
 server.post('/tweets', (req, resp) => {
-  const targetName = req.body.username;
-  const targetAvatar = getUserAvatar(targetName);
-  tweetsArray.push(
-    { username: req.body.username,
+  if (checkRequestInput(req)) {
+    const validInputData = formatInputData(req);
+    const targetAvatar = getUserAvatar(validInputData.username);
+
+    tweetsArray.push( {
+      username: validInputData.username,
       avatar: targetAvatar,
-      tweet: req.body.tweet} );
-  resp.send('OK');
+      tweet: validInputData.tweet
+    });
+    resp.status(200).send('OK');
+  }
+  else {
+    console.log('falta tratar erro no post tweet');
+    resp.sendStatus(501);
+  }
+
 });
 
-server.get('/tweets', (req, resp) => {
-  console.log('tweetsArray: ', tweetsArray);
+server.get('/tweets', ( _, resp) => {
   resp.send(latestTenTweets(tweetsArray));
 });
 
 const serverPort = 5000;
 server.listen(5000, () => {
-  console.log(`server listening, http://localhost:${serverPort}`);
+  console.log(`\n>>> server listening, http://localhost:${serverPort}`);
 });
-
-
-/*
-  { username: "hedgy", avatar: "https://i.pinimg.com/736x/2d/00/2d/2d002d1e5aa86a0b274c4a9bf8a87b53--bubble-baths-hedge-hog.jpg" };
-  */
---
+++ src/services.js
@@ -0,0 +1,152 @@
+const BOB = "bobesponja";
+const BOB_PIC = "https://avatarfiles.alphacoders.com/833/thumb-1920-83315.png";
+const HEDGE = "hedgy";
+const HEDGE_PIC = "https://i.pinimg.com/736x/2d/00/2d/2d002d1e5aa86a0b274c4a9bf8a87b53--bubble-baths-hedge-hog.jpg";
+
+const usersArray = [
+  {
+    username: HEDGE,
+    avatar: HEDGE_PIC
+  },
+  {
+    username: BOB,
+    avatar: BOB_PIC
+  }
+];
+
+const tweetsArray = [
+  {
+    username: BOB,
+    tweet: "summer in the bikini bottom 00",
+    avatar: BOB_PIC
+  },
+  {
+    username: HEDGE,
+    tweet: "I saw the new sonic movie in 2.5x! 01",
+    avatar: HEDGE_PIC
+  },
+  {
+    username: BOB,
+    tweet: "summer in the bikini bottom 02",
+    avatar: BOB_PIC
+  },
+  {
+    username: HEDGE,
+    tweet: "I saw the new sonic movie in 2.5x! 03",
+    avatar: HEDGE_PIC
+  },
+  {
+    username: BOB,
+    tweet: "summer in the bikini bottom 04",
+    avatar: BOB_PIC
+  },
+  {
+    username: HEDGE,
+    tweet: "I saw the new sonic movie in 2.5x! 05",
+    avatar: HEDGE_PIC
+  },
+  {
+    username: BOB,
+    tweet: "summer in the bikini bottom 06",
+    avatar: BOB_PIC
+  },
+  {
+    username: HEDGE,
+    tweet: "I saw the new sonic movie in 2.5x! 07",
+    avatar: HEDGE_PIC
+  },
+  {
+    username: BOB,
+    tweet: "summer in the bikini bottom 08",
+    avatar: BOB_PIC
+  },
+  {
+    username: HEDGE,
+    tweet: "I saw the new sonic movie in 2.5x! 09",
+    avatar: HEDGE_PIC
+  },
+  {
+    username: BOB,
+    tweet: "summer in the bikini bottom 10",
+    avatar: BOB_PIC
+  },
+  {
+    username: HEDGE,
+    tweet: "I saw the new sonic movie in 2.5x! 11",
+    avatar: HEDGE_PIC
+  },
+  {
+    username: BOB,
+    tweet: "summer in the bikini bottom 12",
+    avatar: BOB_PIC
+  },
+  {
+    username: HEDGE,
+    tweet: "I saw the new sonic movie in 2.5x! 13",
+    avatar: HEDGE_PIC
+  },
+  {
+    username: BOB,
+    tweet: "summer in the bikini bottom 14",
+    avatar: BOB_PIC
+  },
+  {
+    username: HEDGE,
+    tweet: "I saw the new sonic movie in 2.5x! 15",
+    avatar: HEDGE_PIC
+  },
+];
+
+function latestTenTweets(targetArray) {
+  if (targetArray.length < 10) {
+    return targetArray.reverse();
+  }
+  else {
+    const size = targetArray.length
+    const tempArray = [];
+    for (let i = size - 1; i > size - 11; i--) {
+      newArray.push(targetArray[i]);
+    }
+    return tempArray;
+  }
+}
+
+function getUserAvatar(targetUserName) {
+  const targetUserObj = usersArray.find( user => (
+    user.username === targetUserName )
+  );
+  return targetUserObj.avatar;
+}
+
+function checkRequestInput(request) {
+  const data = request.body;
+  let validInput = false;
+  
+  if (data.hasOwnProperty('avatar')) {
+    let isEmpty = ( (data.username.trim().replaceAll(' ', '') === "") || (data.avatar.trim() === "") );
+    
+    validInput = !isEmpty;
+  }
+  else {
+    let isEmpty = ( (data.username.trim().replaceAll(' ', '') === "") || (data.tweet.trim() === "") );
+    
+    validInput = !isEmpty;
+  }
+  return validInput;
+}
+
+function formatInputData(request) {
+  const data = request.body;
+  const formattedInput = { username: data.username.trim().replaceAll(' ', '') };
+
+  if (data.hasOwnProperty('avatar')) {
+    formattedInput.avatar = data.avatar.trim().replaceAll(' ', '');
+  }
+  if (data.hasOwnProperty('tweet')) {
+    formattedInput.tweet = data.tweet.trim();
+  }
+  return formattedInput;
+}
+
+
+export { usersArray, tweetsArray, latestTenTweets, getUserAvatar, checkRequestInput, formatInputData };
